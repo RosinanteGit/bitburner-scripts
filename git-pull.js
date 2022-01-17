abb718e94a1b1b4a125c0c9948472285239f5e62
RosinanteGit <97316049+RosinanteGit@users.noreply.github.com>
2022-01-17 20:53:07 +0100

    changed Repository and added omit-folders

--- git-pull.js
+++ git-pull.js
@@ -1,13 +1,13 @@
 let options
 const argsSchema = [
-  ['github', 'alainbryden'],
+  ['github', 'RosinanteGit'],
   ['repository', 'bitburner-scripts'],
   ['branch', 'main'],
   ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
   ['new-file', []], // If a repository listing fails, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
   ['subfolder', 'download'], // Can be set to download to a sub-folder that is not part of the remote repository structure
   ['extension', ['.js', '.ns', '.txt', '.script']], // Files to download by extension
-  ['omit-folder', ['/Temp/']] // Folders to omit
+  ['omit-folder', ['/Temp/', '/Important Scripts/', '/other usefull scripts/', '/bin/', '/lib/', '/math/', '/test/', '']] // Folders to omit
 ]
 
 export function autocomplete (data, args) {
