b4f4eee9ef84e50e1f1f19c57a44aca2846e62ba
RosinanteGit <97316049+RosinanteGit@users.noreply.github.com>
2022-01-28 16:01:08 +0100

    delete some scripts and moved to another folder

--- Important Scripts/test/serverlist.js
+++ Important Scripts/test/serverlist.js
@@ -1,8 +1,8 @@
-/** @param {import("..").NS } ns */
+//** @param {NS} ns **/
+/** @param {import("../..").NS } ns */
 export async function main (ns) {
   var torVorh = false //First things first, do we have a TOR?
   torVorh = ns.getPlayer().tor
-
   if (torVorh == false) {
     //No TOR, return to terminal
     //ns.purchaseTor()
@@ -10,8 +10,9 @@ export async function main (ns) {
     return
   } else {
     //Scan for ALL servers, even those scan.analyze 10 doesnt see from home
-    var scanned = ns.scan('home') //Credit for the base script goes to Wolfwings response to https://www.reddit.com/r/Bitburner/comments/rti82s/release_725_gb_server_crawler_worm_mega_script_v10/
-    var servers = new Set() // Direct Link to it: https://www.reddit.com/r/Bitburner/comments/rti82s/comment/hqt4gkv/?utm_source=share&utm_medium=web2x&context=3
+    var scanned = ns.scan('home')
+    var servers = new Set()
+    var serversFullSet = new Set()
     servers.add('home')
     while (scanned.length > 0) {
       const s = scanned.pop()
@@ -20,48 +21,49 @@ export async function main (ns) {
         scanned = scanned.concat(ns.scan(s))
       }
     }
-    servers.delete('home')
+    serversFullSet = servers //Save a Full scan as backup
+    servers.delete('home') //Removing our own servers
     servers.forEach(
       (
         server //My added part for personal servers
       ) => {
         if (
           server ==
-          'pserv-' +
-            ('0' ||
-              '1' ||
-              '2' ||
-              '3' ||
-              '4' ||
-              '5' ||
-              '6' ||
-              '7' ||
-              '8' ||
-              '9' ||
-              '10' ||
-              '11' ||
-              '12' ||
-              '13' ||
-              '14' ||
-              '15' ||
-              '16' ||
-              '17' ||
-              '18' ||
-              '19' ||
-              '20' ||
-              '21' ||
-              '22' ||
-              '23' ||
-              '24')
+          'daemon' +
+            ('-0' ||
+              '-1' ||
+              '-2' ||
+              '-3' ||
+              '-4' ||
+              '-5' ||
+              '-6' ||
+              '-7' ||
+              '-8' ||
+              '-9' ||
+              '-10' ||
+              '-11' ||
+              '-12' ||
+              '-13' ||
+              '-14' ||
+              '-15' ||
+              '-16' ||
+              '-17' ||
+              '-18' ||
+              '-19' ||
+              '-20' ||
+              '-21' ||
+              '-22' ||
+              '-23' ||
+              '-24')
         ) {
           servers.delete(server)
         }
       }
     )
     servers = Array.from(servers)
-
-    var numberServers = servers.length //Mine from here on
-
+    serversFullSet = Array.from(serversFullSet)
+    var numberServersUsed = servers.length //Mine from here on
+    var numberServers = serversFullSet.length
     var rootHelpers = 0 //vars to determine which servers in the list we can actually root
     var sql = false
     var ssh = false
@@ -90,7 +92,6 @@ export async function main (ns) {
       ssh = true
     }
     servers.forEach(server => {
-      //Again based on Wolfwings, with added code
       if (!ns.hasRootAccess(server)) {
         //accounting for the number of ports, and which
         if (ns.getServerNumPortsRequired(server) <= rootHelpers) {
@@ -110,23 +111,36 @@ export async function main (ns) {
           if (ssh == true) {
             ns.brutessh(server)
           }
-          ns.nuke(server)
-          if (ns.getServerMaxMoney(server) > 0) {
-            //Mine, only hack/grow/weaken cycles for servers
-            ns.exec(server + '.script', 'home', 1500) //that actually have money; and run them with 1500 threads
-          } //on the home server (3,6 TB, enough for 1t==1000b per cycle @my augs)
+          ns.nuke(server)}
+          // if (ns.getServerMaxMoney(server) > 0) {
+          // Mine, only hack/grow/weaken cycles for servers
+          //ns.exec(server + '.script', 'home', 1500) //that actually have money; and run them with 1500 threads
+          // on the home server (3,6 TB, enough for 1t==1000b per cycle @my augs)
           else {
             //Servers without money are subtracted from the count of scripts that
-            numberServers-- //should be running in the end
+            numberServersUsed-- //should be running in the end
           }
-          //await (ns.installBackdoor(server));
+        }
+        //await (ns.installBackdoor(server));
+      } // else if (ns.getServerMaxMoney(server) == 0) {
+        //numberServersUsed--
+      
+    
+    ns.tprint(numberServersUsed + ' / ' + numberServers) //How many scripts shopuld be running(63 for me)
+    servers.forEach(server => {
+      //Check if it ate a script and try to restart it
+      if (
+        !ns.isRunning(server + '.script', 'home') &&
+        ns.getServerMaxMoney(server) > 0
+      ) {
+        ns.tprint('Missing: ' + server + '\n Trying to start it.')
+        ns.exec(server + '.script', 'home', 1500)
+        if (!ns.isRunning(server + '.script', 'home')) {
+          ns.tprint('Nope; HELP!!!')
+        } else {
+          ns.tprint('Success!!!')
         }
       }
-      //else if(ns.getServerMaxMoney(server)==0){
-      //    numberServers--;
-      //}
     })
-    ns.tprint(servers) //Print all servers
-    ns.tprint(numberServers) //How many scripts shopuld be running(63 for me)
   }
 }
